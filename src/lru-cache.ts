59fd7068f95b656570c05913e10c2316da54b7e4
J0hnMilt0n <mjohnmilton41@outlook.com>
2024-07-26 21:42:25 +0530

    initial commit

---
+++ src/lru-cache.ts
@@ -0,0 +1,98 @@
+/**
+ * A Least Recently Used (LRU) cache with Time-to-Live (TTL) support. Items are kept in the cache until they either
+ * reach their TTL or the cache reaches its size and/or item limit. When the limit is exceeded, the cache evicts the
+ * item that was least recently accessed (based on the timestamp of access). Items are also automatically evicted if they
+ * are expired, as determined by the TTL.
+ * An item is considered accessed, and its last accessed timestamp is updated, whenever `has`, `get`, or `set` is called with its key.
+ *
+ * Implement the LRU cache provider here and use the lru-cache.test.ts to check your implementation.
+ * You're encouraged to add additional functions that make working with the cache easier for consumers.
+ */
+
+type LRUCacheProviderOptions = {
+  ttl: number // Time to live in milliseconds
+  itemLimit: number
+}
+type LRUCacheProvider<T> = {
+  has: (key: string) => boolean
+  get: (key: string) => T | undefined
+  set: (key: string, value: T) => void
+}
+interface CachItem {
+  value: any,
+  timeout?: ReturnType<typeof setTimeout>
+}
+interface Cache {
+  [key: string]: CachItem
+}
+
+
+// TODO: Implement LRU cache provider
+export function createLRUCacheProvider<T>({
+  ttl,
+  itemLimit,
+}: LRUCacheProviderOptions): LRUCacheProvider<T> {
+
+  let cache: Cache = {}
+
+  let keyOrder: string[] = []
+
+  function setExpires(key: string) {
+    clearTimeout(cache[key].timeout)
+    cache[key].timeout = setTimeout(() => {
+      delete cache[key]
+    }, ttl)
+  }
+
+  function checkEvict() {
+    let length = Object.keys(cache).length
+    if (length > itemLimit) {
+      let keyToDelete = keyOrder.shift()
+      if (typeof keyToDelete == 'string') {
+        delete cache[keyToDelete]
+      }
+    }
+  }
+
+  function checkLRU(key: string) {
+    let find = keyOrder.findIndex(el => el == key)
+    let newOrder = keyOrder.slice(0, find).concat(keyOrder.slice(find + 1))
+    newOrder.push(key)
+    keyOrder = newOrder
+  }
+
+  return {
+    has: (key: string) => {
+      if (cache[key]) {
+        setExpires(key)
+        checkLRU(key)
+        return true
+      }
+      return false
+    },
+    get: (key: string) => {
+      if (cache[key]) {
+        setExpires(key)
+        checkLRU(key)
+        return cache[key].value
+      }
+      return undefined
+    },
+    set: (key: string, value: T) => {
+      let item: CachItem = {
+        value: value
+      }
+      if (cache[key]) {
+        clearTimeout(cache[key].timeout)
+        let find = keyOrder.findIndex(el => el == key)
+        let newOrder = keyOrder.slice(0, find).concat(keyOrder.slice(find + 1))
+        keyOrder = newOrder
+      }
+      cache[key] = item
+      keyOrder.push(key)
+      setExpires(key)
+      checkEvict()
+      checkLRU(key)
+    },
+  }
+}
